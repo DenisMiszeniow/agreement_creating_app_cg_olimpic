c1512666e94a97bd842aadfcbb0c0e889c352f5b
DenisMiszeniow <duminocorp@gmail.com>
2022-08-12 00:03:15 +0200

    add background to the header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     return (
         <header className={styles.header}>
             <a href="https://cg-olimpic.pl/gimnastyka_artystyczna/">
-                <img width={115} src="https://cg-olimpic.pl/wp-content/uploads/2020/08/Logo_new_web_1.jpg" alt="CG-Olimpic"/>
+                <img width={150} src="https://cg-olimpic.pl/wp-content/uploads/2021/08/logo_nature.png" alt="CG-Olimpic"/>
             </a>
             <h1>Generator Umowy CG-Olimpic</h1>
             <h2>TYLKO DLA URZĄDZEŃ DESKTOPOWYCH (Komputery stacjonarne, Laptopy)</h2>
