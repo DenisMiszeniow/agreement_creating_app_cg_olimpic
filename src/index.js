1ee45ddcedf1b03d7c9c12e95a6912fe3585f683
DenisMiszeniow <duminocorp@gmail.com>
2022-11-07 13:37:32 +0100

    add ScrollToTop logic for routing process

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './BLL/store';
 import { HashRouter } from 'react-router-dom';
+import ScrollTotop from './components/routers/scroll_to_top';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
     <Provider store={store}>
+      <ScrollTotop/>
       <App />
     </Provider>
   </HashRouter>
